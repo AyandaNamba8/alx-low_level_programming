d23e210f683da1e13cc7a26c5b904dbc0773ebdb
AyandaNamba8 <ayanda.namba84@gmail.com>
2022-04-07 15:27:42 -0700

    2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
+	char *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
@@ -20,9 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	p = malloc(nmemb * size);
 
 	if (p == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < (nmemb * size); i++)
 		p[i] = 0;
