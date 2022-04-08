8bcb278854a315c0395ec5d2216079d9e8596b65
AyandaNamba8 <ayanda.namba84@gmail.com>
2022-04-08 13:04:46 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdlib.h>
 /**
  * string_nconcat - concatenates two strings
@@ -10,40 +11,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *nstr;
-	unsigned int len1, len2, size, i, j;
+	char sout;
+	unsigned int ls1, ls2, lsout, i;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	len1 = 0;
-	while (s1[len1] != '\0')
-		len1++;
-	len2 = 0;
-	while (s1[len2] != '\0')
-		len2++;
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
 
-	if (n >= len2)
-		n = len2;
-	size = len1 + n;
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
 
-	nstr = malloc((sizeof(char) * size) + 1);
-	if (nstr == NULL)
+	if (n >= ls2)
+		n = ls2;
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
 		return (NULL);
-	i = 0;
-	while (i < len1)
-	{
-		nstr[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (i < size)
-	{
-		nstr[i] = s2[j];
-		i++;
-		j++;
-	}
-	nstr[i] = '\0';
-return (nstr);
+
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s1[i - ls1];
+
+	sout[i] = '\0';
+return (sout);
 }
