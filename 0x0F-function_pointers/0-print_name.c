6e949a44df7f95bbf04620406e63fdfec757163b
AyandaNamba8 <ayanda.namba84@gmail.com>
2022-04-14 15:19:51 -0700

    0-print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 #include "function_pointers.h"
 /**
  * print_name - prints a name
- * @name: pointer to name.
- * @f: function to pointer
+ * @name: input name.
+ * @f: function to pointer.
  *
  * Return: void.
  */
